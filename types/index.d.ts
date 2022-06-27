aee7c75fe657886bfbb88fcc084eb73d884dc0d4
tzachf <tzachf@wix.com>
2022-06-27 18:20:39 +0300

    add bindRepeaters types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,7 @@
 type PageElementsMapper <T extends keyof PageElementsMap> = T extends `#${infer Suffix}` ? Suffix : never;
 
+
+//RefObject has 2 options - one that counts the elements nickname as the stronger constrain and the other the element properties as the stronger constrain
 type RefsObject2<S extends KeyOfPageElementsMap> =
     {
         [K in S] : K extends KeyOfPageElementsMap ? {
@@ -8,6 +10,7 @@ type RefsObject2<S extends KeyOfPageElementsMap> =
         : never
     }
 
+    //TODO: exclude element whos type of repeaters
 type RefsObject<S extends string> =
     S extends keyof PageElementsMap ? {
         [K in S as PageElementsMapper<K>] :  K extends KeyOfPageElementsMap ? {
@@ -16,8 +19,25 @@ type RefsObject<S extends string> =
     } : never
 
 type KeyOfPageElementsMap = keyof PageElementsMap
-declare function bindView<T extends KeyOfPageElementsMap>(refs: RefsObject< keyof PageElementsMap>): any
+type BindConfigs = {
+    selectorFn? : Function;
+    index? : Number;
+    runOnReady? : Array<Function>
+}
 
+//Initial try to type repeaters RefObjects TODO: make it better
+type RepeatersPageElementsMap<S extends string> =
+    S extends keyof PageElementsMap ?
+        PageElementsMap[S] extends $w.Repeater?{
+            [ K in S as PageElementsMapper<K>] : K extends keyof KeyOfPageElementsMap ? {
+                [P in keyof PageElementsMap[K]] : any
+            }: $w.Repeater
+        } : never
+        : never;
 
+type PageElementsMapFunctionWrapper<S> = ()=> S
 
-declare module 'public/velo-mvvm' { function bindView<T extends KeyOfPageElementsMap>(refs: RefsObject< keyof PageElementsMap>): any}
+declare module '@tzach-f/velo-mvvm' {
+    function bindView<T extends KeyOfPageElementsMap>(refs: RefsObject< keyof PageElementsMap>, config: BindConfigs): any;
+    function bindRepeaters(refs: RepeatersPageElementsMap<keyof PageElementsMap>): any;
+}
